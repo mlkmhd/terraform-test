b740da3b38507f2bd56ee4daeb90bb288afefd61
mehdi <mallakimahdi@gmail.com>
2023-01-25 12:11:00 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,31 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_prefix = "10.0.10.0/24"
 }
+
+resource "azurerm_public_ip" "pip" {
+  name = "book-ip"
+  location = "West Europe"
+  resource_group_name = azurerm_resource_group.rg.name
+  public_ip_address_allocation = "Dynamic"
+  domain_name_label = "bookdevops"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = "book-nic"
+  location = "West Europe"
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name = "bookipconfig"
+    subnet_id = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_storage_account" "stor" {
+  name = "bookstor"
+  location = "West Europe"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
