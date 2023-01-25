9040b3aacc4abd338c27c081a731d3203a72866d
mehdi <mallakimahdi@gmail.com>
2023-01-25 22:54:04 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,63 +1,75 @@
 resource "azurerm_resource_group" "rg" {
-  name = "bookRg"
+  name     = "bookRg"
   location = "West Europe"
-  tags {
-    environment = "Terraform Azure"
-  }
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name = "book-vnet"
-  localtion = "West Europe"
-  address_space = ["10.0.0.0/16"]
+  name                = "book-vnet"
+  location            = "West Europe"
+  address_space       = ["10.0.0.0/16"]
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name = "book-subnet"
+  name                 = "book-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name = azurerm_resource_group.rg.name
-  address_prefix = "10.0.10.0/24"
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.0.10.0/24"]
 }
 
 resource "azurerm_public_ip" "pip" {
-  name = "book-ip"
-  location = "West Europe"
+  name                = "book-ip"
+  location            = "West Europe"
   resource_group_name = azurerm_resource_group.rg.name
-  public_ip_address_allocation = "Dynamic"
-  domain_name_label = "bookdevops"
+  allocation_method   = "Dynamic"
+  domain_name_label   = "bookdevopsmahdi"
 }
 
 resource "azurerm_network_interface" "nic" {
-  name = "book-nic"
-  location = "West Europe"
+  name                = "book-nic"
+  location            = "West Europe"
   resource_group_name = azurerm_resource_group.rg.name
   ip_configuration {
-    name = "bookipconfig"
-    subnet_id = azurerm_subnet.subnet.id
+    name                          = "bookipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.pip.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
 }
 
 resource "azurerm_storage_account" "stor" {
-  name = "bookstor"
-  location = "West Europe"
-  resource_group_name = azurerm_resource_group.rg.name
-  account_tier = "Standard"
+  name                     = "bookstormahdi"
+  location                 = "West Europe"
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "azurerm_linux_virtual_machine" "vm" {
-  name = "bookvm"
-  location = "West Europe"
-  resource_group_name = azurerm_resource_group.rg.name
-  vm_size = "Standard_DS1_v2"
+  name                  = "bookvm"
+  location              = "West Europe"
+  resource_group_name   = azurerm_resource_group.rg.name
+  admin_username        = "mahdi"
+  size                  = "Standard_DS1_v2"
   network_interface_ids = ["${azurerm_network_interface.nic.id}"]
-  storage_image_refrence {
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
     publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "16.04-LTS"
-    version = "latest"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+  admin_ssh_key {
+    username   = "mahdi"
+    public_key = tls_private_key.example_ssh.public_key_openssh
   }
 }
