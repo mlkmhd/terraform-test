13f54d08dd5bd05aecfdb5c83df87a94ccde5095
mehdi <mallakimahdi@gmail.com>
2023-01-28 11:34:06 +0000

    change vm size

--- paas/kubernetes.tf
+++ paas/kubernetes.tf
@@ -13,7 +13,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     name       = "agentpool"
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_B1ms"
     node_count = var.agent_count
   }
   
