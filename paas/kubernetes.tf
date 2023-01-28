042b032c32ee5c44d107112fb33f3be56614edee
mehdi <mallakimahdi@gmail.com>
2023-01-28 07:58:32 +0000

    get kubeconfig

--- paas/kubernetes.tf
+++ paas/kubernetes.tf
@@ -26,3 +26,9 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     type = "SystemAssigned"
   }
 }
+
+resource "local_file" "kubeconfig" {
+  depends_on   = [azurerm_kubernetes_cluster.k8s]
+  filename     = "kubeconfig"
+  content      = azurerm_kubernetes_cluster.k8s.kube_config_raw
+}
