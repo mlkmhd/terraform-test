b987752697da5b22a728c6520dce9d582686e983
mehdi <mallakimahdi@gmail.com>
2023-01-29 09:58:57 +0000

    m

--- v2ray-aci/aci.tf
+++ v2ray-aci/aci.tf
@@ -18,6 +18,14 @@ resource "azurerm_container_group" "example" {
     }
 
     commands = [ "v2ray", "run", "-c", "/etc/v2ray/config.json" ]
+
+    volume {
+      name                 = "config-volume"
+      mount_path           = "/etc/v2ray"
+      storage_account_name = azurerm_storage_account.aci_storage.name
+      storage_account_key  = azurerm_storage_account.aci_storage.primary_access_key
+      share_name           = azurerm_storage_share.container_share.name
+    }
   }
 
   tags = {
