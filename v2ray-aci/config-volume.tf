cb387185385900bb6e12bcdde26086b73436fee4
mehdi <mallakimahdi@gmail.com>
2023-01-29 10:04:54 +0000

    m

--- v2ray-aci/config-volume.tf
+++ v2ray-aci/config-volume.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "aci_storage" {
-  name                     = "v2ray-config-storage"
+  name                     = "v2rayconfigstorage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.resource_group_location
   account_tier             = "Standard"
