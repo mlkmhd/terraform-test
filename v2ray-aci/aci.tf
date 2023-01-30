bee241a2fc9173eef95957ef4f0eebecb6c9ddeb
mehdi <mallakimahdi@gmail.com>
2023-01-30 18:29:48 +0000

    m

--- v2ray-aci/aci.tf
+++ v2ray-aci/aci.tf
@@ -1,16 +1,17 @@
 resource "azurerm_container_group" "example" {
-  name                = "v2ray"
+  for_each = toset( ["1", "2", "3"] )
+  name                = each.key
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-v2ray-tom"
+  dns_name_label      = "aci-v2ray-tom"   ***** 
   os_type             = "Linux"
 
   container {
     name   = "v2ray"
     image  = "v2fly/v2fly-core:latest"
-    cpu    = "0.5"
-    memory = "0.5"
+    cpu    = "0.2"
+    memory = "0.2"
 
     ports {
       port     = 6443
