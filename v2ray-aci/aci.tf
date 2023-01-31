0f318d3cd792d93b9e9c5b46a6bd02890a944aaf
mehdi <mallakimahdi@gmail.com>
2023-01-31 11:41:23 +0000

    m

--- v2ray-aci/aci.tf
+++ v2ray-aci/aci.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_group" "example" {
-  for_each = toset( ["1", "2", "3"] )
+  for_each = range(1,4)
   name                = each.key
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
@@ -11,7 +11,7 @@ resource "azurerm_container_group" "example" {
     name   = "v2ray"
     image  = "v2fly/v2fly-core:latest"
     cpu    = "0.2"
-    memory = "0.2"
+    memory = "0.1"
 
     ports {
       port     = 6443
