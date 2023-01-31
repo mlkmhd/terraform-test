edf491c92228def5d6f4f95c2afc480ccd02af7a
mehdi <mallakimahdi@gmail.com>
2023-01-31 11:48:17 +0000

    m

--- v2ray-aci/aci.tf
+++ v2ray-aci/aci.tf
@@ -1,10 +1,10 @@
 resource "azurerm_container_group" "example" {
-  for_each = range(1,4)
+  count               = 3
   name                = each.key
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-v2ray-tom-${each.key}"
+  dns_name_label      = "aci-v2ray-tom-${count.index}"
   os_type             = "Linux"
 
   container {
