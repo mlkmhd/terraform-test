eafe4387984658bca7c6e681ba4de79f9787f8d6
mehdi <mallakimahdi@gmail.com>
2023-02-05 07:05:15 +0000

    m

--- v2ray-aci/aci.tf
+++ v2ray-aci/aci.tf
@@ -1,10 +1,10 @@
 resource "azurerm_container_group" "example" {
   count               = 1
-  name                = count.index + 1
+  name                = count.index + 2
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-v2ray-tom-${count.index + 1}"
+  dns_name_label      = "aci-v2ray-tom-${count.index + 2}"
   os_type             = "Linux"
 
   container {
