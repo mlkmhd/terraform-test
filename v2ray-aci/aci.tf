a7aec7a9012ac24abeb1c9eeed10af2d000127a5
mahdi <mallakimahdi@gmail.com>
2023-07-10 20:22:19 +0330

    Update aci.tf

--- v2ray-aci/aci.tf
+++ v2ray-aci/aci.tf
@@ -1,10 +1,10 @@
 resource "azurerm_container_group" "example" {
   count               = 1
-  name                = count.index + 12
+  name                = count.index + 13
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "Public"
-  dns_name_label      = "aci-v2ray-tom-${count.index + 12}"
+  dns_name_label      = "aci-v2ray-tom-${count.index + 13}"
   os_type             = "Linux"
 
   container {
